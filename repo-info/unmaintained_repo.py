9107872d22bc3574e8f13415b9f3e8688a782847
kiruthikajanakiraman <kiru2405@gmail.com>
2023-10-25 16:31:53 -0500

    Add last commit date to notes

--- repo-info/unmaintained_repo.py
+++ repo-info/unmaintained_repo.py
@@ -1,5 +1,16 @@
 import csv
 import os
+from github import Github
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--t', type=str, help='GitHub access token to overcome API rate limitations')
+args = parser.parse_args()
+
+GITHUB_API_RATE_LIMIT = 6000
+GITHUB_ACCESS_TOKEN = args.t
+#GITHUB_ACCESS_TOKEN = "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"
+g = Github(GITHUB_ACCESS_TOKEN)
 
 with open('repo-info/repo-info.csv', mode ='r') as file:
 
@@ -16,7 +27,7 @@ input_csv = 'pr-data.csv'
 output_csv = 'temp.csv'
 
 column_to_change = 5  # Change the second column (0-indexed)
-
+counter = 0
 # Open the input CSV file in read mode and the output CSV file in write mode
 with open(input_csv, 'r', newline='') as input_file, open(output_csv, 'w', newline='') as output_file:
     reader = csv.reader(input_file)
@@ -24,10 +35,15 @@ with open(input_csv, 'r', newline='') as input_file, open(output_csv, 'w', newli
 
     for row in reader:
         # Check if the current row has enough columns
+        counter+=1
         if column_to_change < len(row) and row[0] in set:
             # Update the desired column with the new value
+            repo_name = row[0].split('github.com/')[1]
+            repo = Github(GITHUB_ACCESS_TOKEN).get_repo(repo_name)
+            branch = repo.get_branch(repo.default_branch)
+            row[-1] = str(branch.commit.commit.author.date).split(" ")[0]
             row[column_to_change] = "Unmaintained"
         writer.writerow(row)
-
-    os.remove(input_csv)
-    os.rename(output_csv, 'pr-data.csv')
+        print(counter)
+#     os.remove(input_csv)
+#     os.rename(output_csv, 'pr-data.csv')
